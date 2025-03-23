40ba176b6efad32edd12a4297b7ddef8fe80e6f2
sussiniguanziroli <sussiniguanziroli@gmail.com>
2025-03-23 19:07:21 -0300

    commit comenzando el tp1

---
+++ Programacion 1/Modulo1/tp1.py
@@ -0,0 +1,23 @@
+# 1 - Crear un programa que imprima por pantalla el mensaje: “Hola Mundo!”
+print("Hola Mundo")
+
+# 2 - Crear un programa que pida al usuario su nombre e imprima por pantalla un saludo usando
+#el nombre ingresado. Por ejemplo: si el usuario ingresa “Marcos”, el programa debe imprimir
+#por pantalla “Hola Marcos!”. Consejo: esto será más sencillo si utilizas print(f…) para
+#realizar la impresión por pantalla.
+nombre = input("Ingresa tu nombre y recibe un saludo: ")
+print(f"Hola {nombre}!")
+
+# 3 - Crear un programa que pida al usuario su nombre, apellido, edad y lugar de residencia e
+#imprima por pantalla una oración con los datos ingresados. Por ejemplo: si el usuario ingresa
+#“Marcos”, “Pérez”, “30” y “Argentina”, el programa debe imprimir “Soy Marcos Pérez, tengo 30
+#años y vivo en Argentina”. Consejo: esto será más sencillo si utilizas print(f…) para realizar
+#la impresión por pantalla.
+nombre = input("Ingrese su nombre: ")
+apellido = input("Ingrese su apellido: ")
+edad = int(input("Ingrese su edad: "))
+residencia = input("Ingrese su lugar de residencia: ")
+print(f"Hola! Soy {nombre} {apellido}, tengo {edad} años y vivo en {residencia}")
+
+#4) Crear un programa que pida al usuario el radio de un círculo e imprima por pantalla su área y
+#su perímetro.
