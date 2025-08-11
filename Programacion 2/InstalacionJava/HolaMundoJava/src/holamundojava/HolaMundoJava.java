5dc827d560507f48d7bcdef525c104c64900d9e1
sussiniguanziroli <sussiniguanziroli@gmail.com>
2025-08-11 18:20:24 -0300

    elimino el repo embebido

--- Programacion 2/InstalacionJava/HolaMundoJava/src/holamundojava/HolaMundoJava.java
+++ Programacion 2/InstalacionJava/HolaMundoJava/src/holamundojava/HolaMundoJava.java
@@ -14,7 +14,8 @@ public class HolaMundoJava {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        System.out.println("Hola Mundo");
+        String saludo = "Hola " + "Mundo";
+        System.out.println(saludo);
     }
     
 }
